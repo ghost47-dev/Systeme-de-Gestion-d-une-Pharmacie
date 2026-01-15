5667e8c809a926ce4c9f22185465c53aa215b7eb
Ghost <robertpetit730@gmail.com>
2026-01-15 23:05:53 +0100

    saved ProductManager from destruction

---
+++ src/main/java/com/gestionpharmacie/managers/ProductManager.java
@@ -0,0 +1,40 @@
+package com.gestionpharmacie.managers;
+
+import com.gestionpharmacie.model.Product;
+
+import java.util.ArrayList;
+
+public class ProductManager {
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        if (fetchProduct(product.getId()) != null) {
+            System.out.println("This product already exits!"); // will become an exception soon !!
+        }
+        products.add(product);
+    }
+    public Product fetchProduct(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+    public void updateProduct(int id, String newName, double newPrice, int newQuantity) {
+        Product product = fetchProduct(id);
+        if (product == null) {
+            System.out.println("This product doesn't exist!"); // turn to exception!
+        }
+        product.setName(newName);
+        product.setPrice(newPrice);
+        product.setQuantity(newQuantity);
+    }
+    public void deleteProduct(Product product) {
+        if (fetchProduct(product.getId()) == null) {
+            System.out.println("This product doesn't exists!");
+        } else {
+            products.remove(product);
+        }
+    }
+}
---
+++ src/main/java/com/gestionpharmacie/managers/UserManager.java
@@ -0,0 +1,5 @@
+package com.gestionpharmacie.managers;
+
+public class UserManager {
+
+}
