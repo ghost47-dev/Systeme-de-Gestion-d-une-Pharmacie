185b3a13db6ff0d8d1791da30815e142462906b7
Ghost <robertpetit730@gmail.com>
2026-01-15 23:12:36 +0100

    added UserManager and properties for User

--- src/main/java/com/gestionpharmacie/managers/UserManager.java
+++ src/main/java/com/gestionpharmacie/managers/UserManager.java
@@ -1,5 +1,21 @@
 package com.gestionpharmacie.managers;
 
-public class UserManager {
+import com.gestionpharmacie.model.User;
 
+import java.util.ArrayList;
+
+public class UserManager {
+    private ArrayList<User> users = new ArrayList<>();
+
+    public User fetchUser(String login) {
+        for (User user : users) {
+            if (user.getLogin().equals(login)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    public void addUser(User user) {
+        users.add(user);
+    }
 }
--- src/main/java/com/gestionpharmacie/model/User.java
+++ src/main/java/com/gestionpharmacie/model/User.java
@@ -1,4 +1,31 @@
 package com.gestionpharmacie.model;
 
 public class User {
+    private String login;
+    private String password;
+    private String privelage;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPrivelage() {
+        return privelage;
+    }
+
+    public void setPrivelage(String privelage) {
+        this.privelage = privelage;
+    }
 }
