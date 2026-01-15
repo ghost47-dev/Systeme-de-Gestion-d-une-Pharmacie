6d63082c58c6b7dcc7b4a7cc1698f1643ca7fa05
awss1236 <awsssayah@gmail.com>
2026-01-15 22:30:42 +0100

    brrr

--- src/main/java/com/gestionpharmacie/Main.java
+++ src/main/java/com/gestionpharmacie/Main.java
@@ -1,10 +1,38 @@
 package com.gestionpharmacie;
 import java.util.ArrayList;
 import java.util.Scanner;
+import com.gestionpharmacie.model.Product;
+import com.gestionpharmacie.model.User;
 
 public class Main {
+    void printCommands(){
+        System.out.println("1. :w
+                );
+    }
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("schesting...");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Login:");
+        String login = sc.nextLine();
+        System.out.println("Password:");
+        String password = sc.nextLine();
+
+        User u; // tabbasi lazem User manager.
+        boolean found = true;
+        if(!found){
+            System.err.println("User not found!");
+            return;
+        }
+
+        while(true){
+            int choice = sc.nextInt();
+            System.out.println();
+            switch (choice) {
+                case 1:
+                    break;
+                default:
+                    System.err.println("Invalid command!");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/com/gestionpharmacie/model/Product.java
+++ src/main/java/com/gestionpharmacie/model/Product.java
@@ -1,4 +1,23 @@
 package com.gestionpharmacie.model;
 
 public class Product {
+    private int id;
+    private String nom;
+    private int quantite;
+
+	public int getId() {
+		return id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
 }
