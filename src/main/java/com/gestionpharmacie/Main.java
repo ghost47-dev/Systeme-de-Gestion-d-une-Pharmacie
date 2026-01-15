41b0e1a84d908697e455a521efa15f985ca8c9e1
awss1236 <awsssayah@gmail.com>
2026-01-15 22:55:59 +0100

    did some main shit

--- src/main/java/com/gestionpharmacie/Main.java
+++ src/main/java/com/gestionpharmacie/Main.java
@@ -5,12 +5,34 @@ import com.gestionpharmacie.model.Product;
 import com.gestionpharmacie.model.User;
 
 public class Main {
-    void printCommands(){
-        System.out.println("1. :w
-                );
+    private static Scanner sc;
+    static void handleProductRelatedRequest(){
+        System.out.println("1. Add.");
+        System.out.println("2. Modify.");
+        System.out.println("3. Remove.");
+        int choice = sc.nextInt();
+        if(choice == 1){
+        }else if(choice == 2){
+        }else if(choice == 3){
+        }else{
+            System.err.println("Invalid choice!");
+        }
     }
+
+    static void handleSupplierRelatedRequest(){
+        System.out.println("1. Add.");
+        System.out.println("2. Modify.");
+        System.out.println("3. Remove.");
+    }
+
+    static void handleClientRelatedRequest(){
+    }
+
+    static void handleAdminRelatedRequest(){
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         System.out.println("Login:");
         String login = sc.nextLine();
         System.out.println("Password:");
@@ -24,14 +46,26 @@ public class Main {
         }
 
         while(true){
-            int choice = sc.nextInt();
-            System.out.println();
-            switch (choice) {
-                case 1:
-                    break;
-                default:
-                    System.err.println("Invalid command!");
-                    break;
+            int choice;
+            System.out.println("1. Product.");
+            System.out.println("2. Supplier.");
+            System.out.println("3. Client.");
+            System.out.println("4. Analysis.");
+            choice = sc.nextInt();
+            if(choice == 1){
+                handleProductRelatedRequest();
+            }else if(choice == 2){
+                handleSupplierRelatedRequest();
+            }else if(choice == 3){
+                handleClientRelatedRequest();
+            }else if(choice == 4){
+                if(u.getPrivelige().equals("Admin")){
+                    handleAdminRelatedRequest();
+                }else{
+                    System.err.println("Unpriveliged user!");
+                }
+            }else{
+                System.err.println("Invalid choice!");
             }
         }
     }
--- src/main/java/com/gestionpharmacie/managers/ProductManager.java
+++ src/main/java/com/gestionpharmacie/managers/ProductManager.java
@@ -8,16 +8,17 @@ public class ProductManager {
     private ArrayList<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        if (fetshProduct(product.getId()) {
+        if (fetchProduct(product.getId()) != null) {
             System.out.println("This product already exits!");
         }
         products.add(product);
     }
-    public Product fetshProduct(int id) {
+    public Product fetchProduct(int id) {
         for (Product product : products) {
             if (product.getId() == id) {
                 return product;
             }
         }
+        return null;
     }
 }
