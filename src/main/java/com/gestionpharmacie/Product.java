32c087834569611f369b7758ad67915c53949ccf
Ghost <robertpetit730@gmail.com>
2026-01-15 18:47:06 +0100

    implemented minimal add product method

--- src/main/java/com/gestionpharmacie/Product.java
+++ src/main/java/com/gestionpharmacie/Product.java
@@ -4,6 +4,11 @@ public class Product {
     private int id;
     private String name;
 
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/com/gestionpharmacie/ProductManager.java
+++ src/main/java/com/gestionpharmacie/ProductManager.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 
 public class ProductManager {
     private ArrayList<Product> products = new ArrayList<>();
-
+    void addProduct(Product p) {
+        for (Product product : products) {
+            if (product.getId() == p.getId()) {
+                System.out.println("This product already exits");
+                return;
+            }
+        }
+        products.add(p);
+    }
 }
