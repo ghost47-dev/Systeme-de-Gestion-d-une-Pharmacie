a6e71a4e7bf5d071119fc67dd9882ec79d5bf648
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-01-21 16:27:53 +0100

    tabassi

--- src/main/java/com/gestionpharmacie/utilities/DatabaseConnection.java
+++ src/main/java/com/gestionpharmacie/utilities/DatabaseConnection.java
@@ -8,8 +8,8 @@ public class DatabaseConnection {
     private static Connection connection;
 
     private static final String URL = "jdbc:mysql://localhost:3306/pharmacy";
-    private static final String USER = "root";
-    private static final String PASSWORD = "admin";
+    private static final String USER = "malek";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() {
             try {
