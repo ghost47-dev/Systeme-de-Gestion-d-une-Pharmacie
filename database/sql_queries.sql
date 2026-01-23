3dbf7b6a3c26a3d6ca3105fc39c95b44027330f2
awss1236 <awsssayah@gmail.com>
2026-01-23 21:30:53 +0100

    oops

--- database/sql_queries.sql
+++ database/sql_queries.sql
@@ -23,7 +23,7 @@ create table product
 create table sale
 (
     id        int auto_increment primary key,
-    total_price int,
+    total_price double,
     client_id int null,
     constraint sale_client_id_fk
         foreign key (client_id) references client (id)
