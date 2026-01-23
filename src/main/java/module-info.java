cc2ab19380e1e149bb816d2aab5f508a0faebc71
awss1236 <awsssayah@gmail.com>
2026-01-23 04:16:00 +0100

    small changes to stop some compiler warnings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.gestionpharmacie {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml;
-    requires java.sql;
+    requires transitive java.sql;
 
 
     opens com.gestionpharmacie to javafx.fxml;
